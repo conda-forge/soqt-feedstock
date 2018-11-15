8751bb05d4d6d52dc79c9920fc759225198d9b4d
looooo <sppedflyer@gmail.com>
2018-11-15 07:52:10 +0100

    update recipe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ cmake -G "Ninja" ^
       -D CMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_BUILD_TYPE="Release" ^
-      -D USE_QT5=ON ^
+      -D SOQT_USE_QT5=ON ^
       ..
 
 if errorlevel 1 exit 1
