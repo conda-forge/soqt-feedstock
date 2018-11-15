42a932d8a8c7525715c47f708fefe34e72453d9d
lorenz <looooo@users.noreply.github.com>
2018-11-15 08:47:50 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,6 +6,7 @@ cmake -G "Ninja" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_BUILD_TYPE="Release" ^
       -D SOQT_USE_QT5=ON ^
+      -D SOQT_BUILD_DOCUMENTATION:BOOL=NO ^
       ..
 
 if errorlevel 1 exit 1
