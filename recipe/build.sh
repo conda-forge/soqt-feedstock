dbce464fca19c14b762242326005c1fd672c8b4c
lorenz <looooo@users.noreply.github.com>
2022-05-01 10:46:33 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,6 @@ cd build
 cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_PREFIX_PATH=$PREFIX \
-      -D CMAKE_INSTALL_LIBDIR=$PREFIX/lib \
       -D CMAKE_BUILD_TYPE="Release" \
       -D SOQT_USE_QT5=ON \
       -D SOQT_BUILD_DOCUMENTATION:BOOL=NO \
