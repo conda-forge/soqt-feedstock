0657c1ebf4907a419bb26463d4655c6eff8a1460
lorenz <looooo@users.noreply.github.com>
2023-11-09 19:29:10 +0100

    use qt6

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ cmake -G "Ninja" ^
       -D CMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_BUILD_TYPE="Release" ^
-      -D SOQT_USE_QT5=ON ^
+      -D SOQT_USE_QT6=ON ^
       -D SOQT_BUILD_DOCUMENTATION:BOOL=NO ^
       ..
 
