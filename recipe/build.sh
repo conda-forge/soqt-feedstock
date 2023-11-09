bb7c873a89d58c3ae7f960f71ef7a776ba7f0ebe
lorenz <looooo@users.noreply.github.com>
2023-11-09 19:28:53 +0100

    use qt6

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_PREFIX_PATH=$PREFIX \
       -D CMAKE_BUILD_TYPE="Release" \
-      -D SOQT_USE_QT5=ON \
+      -D SOQT_USE_QT6=ON \
       -D SOQT_BUILD_DOCUMENTATION:BOOL=NO \
       ..
 
