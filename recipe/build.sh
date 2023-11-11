6146f5533a43507f3e742536003454501b80dc94
lorenz <looooo@users.noreply.github.com>
2023-11-11 15:04:33 +0100

    add qt_host_path

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,7 @@ cmake -G "Ninja" \
       -D CMAKE_BUILD_TYPE="Release" \
       -D SOQT_USE_QT6=ON \
       -D SOQT_BUILD_DOCUMENTATION:BOOL=NO \
+      -D QT_HOST_PATH=${PREFIX} \
       ..
 
 ninja install
