16c269e61800be0ee23b571bb81db3f4725eba09
looooo <sppedflyer@gmail.com>
2023-11-11 15:52:55 +0100

    qtselector

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,22 @@
 mkdir -p build
 cd build
 
+declare -a QT_SELECTOR
+
+if [[ "$build_variant" == "qt5" ]]; then
+      QT_SELECTOR+=(-D SOQT_USE_QT5=ON)
+fi
+if [[ "$build_variant" == "qt6" ]]; then
+      QT_SELECTOR+=(-D SOQT_USE_QT6=ON)
+fi
+
 cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_PREFIX_PATH=$PREFIX \
       -D CMAKE_BUILD_TYPE="Release" \
-      -D SOQT_USE_QT6=ON \
       -D SOQT_BUILD_DOCUMENTATION:BOOL=NO \
       -D QT_HOST_PATH=${PREFIX} \
+      ${QT_SELECTOR[@]} \
       ..
 
 ninja install
