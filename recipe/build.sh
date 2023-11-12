bb77b45f03a2396a461771c04e1d163866f96ba5
looooo <sppedflyer@gmail.com>
2023-11-12 20:39:09 +0100

    simplefy script and fix missing qt-main dependency for the build-env

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,29 +1,26 @@
+#!/bin/bash
+
+# Create the build directory
 mkdir -p build
 cd build
 
-QT_SELECTOR=()
-
+# Set QT_SELECTOR based on the build variant
+QT_SELECTOR=""
 if [[ "$build_variant" = "qt5" ]]; then
-      QT_SELECTOR+=("-D SOQT_USE_QT5=ON")
-fi
-if [[ "$build_variant" = "qt6" ]]; then
-      QT_SELECTOR+=("-D SOQT_USE_QT6=ON")
-fi
-
-# preventing the build from using the wrong build of qt.
-# Qt tools for the build should be used from the BUILD_PREFIX
-if [[ ${target_platform} != ${build_platform} ]]; then
-      rm -f ${PREFIX}/lib/qt6/moc
-      ln -s ${BUILD_PREFIX}/lib/qt6/moc ${PREFIX}/lib/qt6/moc
+    QT_SELECTOR="-D SOQT_USE_QT5=ON"
+elif [[ "$build_variant" = "qt6" ]]; then
+    QT_SELECTOR="-D SOQT_USE_QT6=ON"
 fi
 
+# Run CMake with specified options
 cmake -G "Ninja" \
-      -D CMAKE_INSTALL_PREFIX=$PREFIX \
-      -D CMAKE_PREFIX_PATH=$PREFIX \
-      -D CMAKE_BUILD_TYPE="Release" \
-      -D SOQT_BUILD_DOCUMENTATION:BOOL=NO \
-      -D QT_HOST_PATH=${PREFIX} \
-      "${QT_SELECTOR[@]}" \
-      ..
+    -D CMAKE_INSTALL_PREFIX="$PREFIX" \
+    -D CMAKE_PREFIX_PATH="$PREFIX" \
+    -D CMAKE_BUILD_TYPE="Release" \
+    -D SOQT_BUILD_DOCUMENTATION:BOOL=NO \
+    -D QT_HOST_PATH="${PREFIX}" \
+    ${QT_SELECTOR} \
+    ..
 
+# Build and install using Ninja
 ninja install
