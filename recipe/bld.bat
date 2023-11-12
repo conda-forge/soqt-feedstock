2a176c489b77a0adedb107177e80b2e5ff4a17a3
lorenz <looooo@users.noreply.github.com>
2023-11-12 13:42:14 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,23 @@
 mkdir build
 cd build
 
+set "QT_SELECTOR="
+
+if "%build_variant%"=="qt5" (
+    set "QT_SELECTOR=-D SOQT_USE_QT5=ON"
+)
+
+if "%build_variant%"=="qt6" (
+    set "QT_SELECTOR=-D SOQT_USE_QT6=ON"
+)
+
 cmake -G "Ninja" ^
       -D CMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_BUILD_TYPE="Release" ^
       -D SOQT_USE_QT6=ON ^
       -D SOQT_BUILD_DOCUMENTATION:BOOL=NO ^
+      %QT_SELECTOR% ^
       ..
 
 if errorlevel 1 exit 1
