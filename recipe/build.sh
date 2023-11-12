5801aa02c3362edaacf7ec1d594dd7c3f6509bb2
looooo <sppedflyer@gmail.com>
2023-11-12 20:23:00 +0100

    try to use the build-qt for cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,20 @@
 mkdir -p build
 cd build
 
-declare -a QT_SELECTOR
+QT_SELECTOR=()
 
 if [[ "$build_variant" = "qt5" ]]; then
-      QT_SELECTOR+=(-D SOQT_USE_QT5=ON)
+      QT_SELECTOR+=("-D SOQT_USE_QT5=ON")
 fi
 if [[ "$build_variant" = "qt6" ]]; then
-      QT_SELECTOR+=(-D SOQT_USE_QT6=ON)
+      QT_SELECTOR+=("-D SOQT_USE_QT6=ON")
+fi
+
+# preventing the build from using the wrong build of qt.
+# Qt tools for the build should be used from the BUILD_PREFIX
+if [[ ${target_platform} != ${build_platform} ]]; then
+      rm -f ${PREFIX}/lib/qt6/moc
+      ln -s ${BUILD_PREFIX}/lib/qt6/moc ${PREFIX}/lib/qt6/moc
 fi
 
 cmake -G "Ninja" \
@@ -16,7 +23,7 @@ cmake -G "Ninja" \
       -D CMAKE_BUILD_TYPE="Release" \
       -D SOQT_BUILD_DOCUMENTATION:BOOL=NO \
       -D QT_HOST_PATH=${PREFIX} \
-      ${QT_SELECTOR[@]} \
+      "${QT_SELECTOR[@]}" \
       ..
 
 ninja install
