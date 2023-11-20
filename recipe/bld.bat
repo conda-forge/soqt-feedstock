8de74093332ed17cd51871d230c41c7d67375eb8
lorenz <looooo@users.noreply.github.com>
2023-11-20 22:30:03 +0100

    soft 1.6.1, qt6 (#18)
    * qt6
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.28.0, and conda-forge-pinning 2023.11.09.13.45.39
    
    * use latest commit
    
    * use qt6
    
    * use qt6
    
    * Update sha256sum
    
    * remove patch, is included in newer sources
    
    * update patch
    
    * add data source
    
    * add another source
    
    * clean
    
    * update sha256sum of soanydata
    
    * Update sha256
    
    * update subrepo dir
    
    * update dir
    
    * update patch
    
    * add qt-build-dep for cross-compiuling
    
    * add config file
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.28.0, and conda-forge-pinning 2023.11.09.20.47.34
    
    * stuff
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.28.0, and conda-forge-pinning 2023.11.10.07.51.14
    
    * Update conda-forge.yml
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.28.0, and conda-forge-pinning 2023.11.10.07.51.14
    
    * add qt_host_path
    
    * build-variant qt-main, qt6-main
    
    * update build variants
    
    * try another attempt for build-variants
    
    * build_variant
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.28.0, and conda-forge-pinning 2023.11.11.11.53.43
    
    * increase build number
    
    * qtselector
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update bld.bat
    
    * Update bld.bat
    
    * Update conda-forge.yml
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.28.0, and conda-forge-pinning 2023.11.12.08.19.38
    
    * Update conda-forge.yml
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.28.0, and conda-forge-pinning 2023.11.12.08.19.38
    
    * Update build.sh
    
    * Update build.sh
    
    * try to use the build-qt for cross-compiling
    
    * simplefy script and fix missing qt-main dependency for the build-env
    
    * try moving moc from build to host (osx-arm64)
    
    * update build-scripts
    
    * chatgpt corrections
    
    * name the package soqt6 when build with qt6
    
    * name_suffix
    
    * Update recipe/meta.yaml
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * update to version 1.6.1
    
    * fix url, reset build-number
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,28 @@
 mkdir build
 cd build
 
+set "QT_SELECTOR="
+
+if "%build_variant%"=="qt5" (
+    set "QT_SELECTOR=-D SOQT_USE_QT5=ON"
+    echo QT_SELECTOR set for qt5: %QT_SELECTOR%
+)
+
+if "%build_variant%"=="qt6" (
+    set "QT_SELECTOR=-D SOQT_USE_QT6=ON"
+    echo QT_SELECTOR set for qt6: %QT_SELECTOR%
+)
+
+if "%QT_SELECTOR%"=="" (
+    echo Unknown build variant: %build_variant%. QT_SELECTOR remains unset.
+)
+
 cmake -G "Ninja" ^
       -D CMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_BUILD_TYPE="Release" ^
-      -D SOQT_USE_QT5=ON ^
       -D SOQT_BUILD_DOCUMENTATION:BOOL=NO ^
+      %QT_SELECTOR% ^
       ..
 
 if errorlevel 1 exit 1
